9b33d365468a95797240f2be09e355f032af41dc
Davies Esiro <ojurereoluwad@gmail.com>
2020-05-12 23:57:20 +0100

    firebase configuration

--- src/App.js
+++ src/App.js
@@ -7,20 +7,40 @@ import Header from './components/header/header.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import HomePage from './pages/homepage/homepage.component';
+import {auth} from './firebase/firebase.utils';
 
+class App extends React.Component {
+  constructor(){
+    super();
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={ShopPage} />
-        <Route exact path='/signin' component={SignInAndSignUpPage} />
-      </Switch>
-      
-    </div>
-  );
+    this.state = {
+        currentUser:null
+    }
+  }
+
+  componentDidMount(){
+    auth.onAuthStateChanged(user=>{
+      this.setState({currentUser:user});
+
+      console.log(user);
+    });
+  }
+
+  render(){
+
+    return (
+      <div className="App">
+        <Header/>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/shop' component={ShopPage} />
+          <Route exact path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
+        
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+const config = {
+    apiKey: "AIzaSyD7y7t47045lD4b4XMRBXfG4Ttx8oOlhAM",
+    authDomain: "crwn-db-cac19.firebaseapp.com",
+    databaseURL: "https://crwn-db-cac19.firebaseio.com",
+    projectId: "crwn-db-cac19",
+    storageBucket: "crwn-db-cac19.appspot.com",
+    messagingSenderId: "26003333446",
+    appId: "1:26003333446:web:0b9cb59e7ecad48d934547",
+    measurementId: "G-F67J7W8D18"
+  };
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({prompt:'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
